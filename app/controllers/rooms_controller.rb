b193d7d7fe26afe25d291961bb7cae082610dc37
nan56koko26-ai <nan56koko26@gmail.com>
2026-03-11 15:22:14 +0900

    メッセージ送信機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,23 +1,23 @@
-class RoomsController < ApplicationController
-  def index
-  end
-
-  def new
-    @room = Room.new
+class MessagesController < ApplicationController
+ def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
   end
 
   def create
-    @room = Room.new(room_params)
-    if @room.save
-      redirect_to root_path
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
     else
-      render :new, status: :unprocessable_entity
+      render :index, status: :unprocessable_entity
     end
   end
 
+
   private
 
-  def room_params
-    params.require(:room).permit(:name, user_ids: [])
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
